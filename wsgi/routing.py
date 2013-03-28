f92b015047224fbaf10e7912cd64063ab509f780
mb01 <mbowden01@gmail.com>
2013-03-28 21:36:03 +0900

    another bug

--- wsgi/routing.py
+++ wsgi/routing.py
@@ -26,4 +26,5 @@ def router(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
     start_response(status, response_headers)
+    return response_body
     
